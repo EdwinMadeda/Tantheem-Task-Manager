3b73e9bd140c20eb6677a5e09d7d9a6cb2ffbdf2
EdwinMadeda <edwinmadeda@outlook.com>
2023-02-03 20:57:10 +0300

    second commit

--- src/ReusableComponents/AddBtn.js
+++ src/ReusableComponents/AddBtn.js
@@ -1,10 +1,12 @@
-import { BsPlus } from "react-icons/bs";
+// import { BsPlus } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa";
 import CustomLink from "./CustomLink";
 
 const AddBtn = ({ label, path, state = {} }) => {
   return (
     <CustomLink className="addBtn btn" to={path} state={state}>
       {/* <BsPlus className="icon" /> */}
+      <FaPlus className="icon" />
       <p className="label">{label}</p>
     </CustomLink>
   );
